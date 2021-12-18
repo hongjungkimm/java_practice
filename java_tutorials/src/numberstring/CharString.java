9efee6080cdeeb511678e2967ee7507dd1142ecc
HongJungKim <hong267@naver.com>
2021-12-19 00:53:27 +0900

    update java_tutorials

---
+++ java_tutorials/src/numberstring/CharString.java
@@ -0,0 +1,14 @@
+package numberstring;
+
+public class CharString {
+
+	public static void main(String[] args) {
+		
+		System.out.println('생');
+		System.out.println("생활코딩");
+		System.out.println("생활코딩"+"입니다.");
+		System.out.println("egoing said \"Welcome programming world\"");
+
+	}
+
+}
---
+++ java_tutorials/src/numberstring/Number.java
@@ -0,0 +1,11 @@
+package numberstring;
+
+public class Number {
+
+	public static void main(String[] args) {
+		
+		System.out.println(1.2+2.3);
+		
+	}
+
+}
